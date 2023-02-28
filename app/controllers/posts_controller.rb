91b4c61bf594d92a4c43a8301b0b10fc41ef2c16
Mikel Andzelo <mdybka03@gmail.com>
2023-03-01 00:54:28 +0100

    posts with photos!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -102,6 +102,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :body , :views, :user_id)
+      params.require(:post).permit(:title, :body , :photo, :views, :user_id)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   # active storage
   has_one_attached :thumbnail
   has_one_attached :banner
+  has_one_attached :photo
   #action text
   has_rich_text :body
   validates :title, length: {minimum: 5, maximum: 125}
