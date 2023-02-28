f6b6c320cb7df9ab55bfcaa8fdf3496b5a09ce39
Mikel Andzelo <mdybka03@gmail.com>
2023-03-01 00:21:49 +0100

    security fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,13 +9,22 @@ class PostsController < ApplicationController
 
   # GET /posts/1 or /posts/1.json
   def show
-    # Check if user has already viewed the post
-    if cookies["post_#{params[:id]}"] != 'true'
-      # If not, increment views by 1 and set cookie to indicate the post has been viewed
-      @post.increment!(:views)
-      cookies["post_#{params[:id]}"] = 'true'
+    if Current.user
+      if cookies["post_#{params[:id]}"] != 'true'
+        # Find the post
+        @post = Post.find(params[:id])
+  
+        # If found, increment views by 1 and set cookie to indicate the post has been viewed
+        if @post.present?
+          @post.increment!(:views)
+          cookies["post_#{params[:id]}"] = 'true'
+        end
+      end
+    else 
+      redirect_to root_path, alert: "Please log in to continue"
     end
   end
+  
   
   
 
@@ -26,7 +35,18 @@ class PostsController < ApplicationController
 
   # GET /posts/1/edit
   def edit
+    if Current.user # check if a user is logged in
+      if @post.user_id == Current.user.id # check if the post belongs to the current user
+        render :edit
+      else
+        redirect_to post_path(@post), alert: "You can only edit your own posts."
+      end
+    else 
+      redirect_to root_path, alert: "Please log in to continue"
+    end
   end
+  
+  
 
   # POST /posts or /posts.json
   def create
@@ -62,13 +82,17 @@ class PostsController < ApplicationController
 
   # DELETE /posts/1 or /posts/1.json
   def destroy
-    @post.destroy
-
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
-      format.json { head :no_content }
+    if @post.user_id == Current.user.id # check if the post belongs to the current user
+      @post.destroy
+      respond_to do |format|
+        format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
+        format.json { head :no_content }
+      end
+    else
+      redirect_to post_path(@post), alert: "You can only delete your own posts."
     end
   end
+  
 
   private
     # Use callbacks to share common setup or constraints between actions.
